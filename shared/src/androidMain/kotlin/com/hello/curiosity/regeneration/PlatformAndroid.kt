95704a51fa4119f1ec6cf4cbc650ecea19db4c20
Kyle <[hopeman15@users.noreply.github.com]>
2024-03-08 21:16:26 +0100

    fix shared issues

--- shared/src/androidMain/kotlin/com/hello/curiosity/regeneration/PlatformAndroid.kt
+++ shared/src/androidMain/kotlin/com/hello/curiosity/regeneration/PlatformAndroid.kt
@@ -1,7 +1,7 @@
 package com.hello.curiosity.regeneration
 
-class AndroidPlatform : Platform {
+class PlatformAndroid : Platform {
     override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
 }
 
-actual fun getPlatform(): Platform = AndroidPlatform()
+actual fun getPlatform(): Platform = PlatformAndroid()
