95208d673527d13905b15847b81bfa7d53ca3022
Kyle <[hopeman15@users.noreply.github.com]>
2024-03-08 18:16:17 +0100

    dependency and configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
+import io.gitlab.arturbosch.detekt.Detekt
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     //trick: for the same plugin versions in all sub-modules
     alias(libs.plugins.androidApplication).apply(false)
     alias(libs.plugins.androidLibrary).apply(false)
+    alias(libs.plugins.detekt)//.apply(false)
     alias(libs.plugins.kotlinAndroid).apply(false)
     alias(libs.plugins.kotlinMultiplatform).apply(false)
 }
@@ -11,5 +13,25 @@ plugins {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         jvmTarget = "17"
+    }
+}
+
+allprojects {
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+
+    detekt {
+//        toolVersion = libs.versions.detekt.get()
+        config.setFrom("$rootDir/detekt/detekt.yml")
+        buildUponDefaultConfig = true
+    }
+}
+
+tasks.withType<Detekt>().configureEach {
+    reports {
+        xml.required.set(false)
+        html.required.set(true)
+        txt.required.set(false)
+        sarif.required.set(false)
+        md.required.set(false)
     }
 }
